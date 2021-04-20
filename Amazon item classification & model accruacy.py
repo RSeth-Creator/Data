079f519ae8315997327c3f48d3396013f7fb805f
RAJ KUMAR SETH <71996831+RSeth-Creator@users.noreply.github.com>
2021-04-20 10:24:21 +0530

    Add files via upload

--- Amazon item classification & model accruacy.py
+++ Amazon item classification & model accruacy.py
@@ -44,40 +44,46 @@ fig = plt.figure(figsize=(8,6))
 df.groupby('category').product_title_desc.count().plot.bar(ylim=0)
 plt.show()
 
-
+#Removing Stop words set the catageory in 0,1,2,3,4,5....
 from sklearn.feature_extraction.text import TfidfVectorizer
 tfidf = TfidfVectorizer( ngram_range=(1, 2), stop_words='english')
 features = tfidf.fit_transform(df.product_title_desc).toarray()
 labels = df.category_id
 features.shape
 
-#Applying naive bay's model to the dataset
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
 
+#Train and test data set created below 
 X_train, X_test, y_train, y_test = train_test_split(df['product_title_desc'], df['category'], random_state = 0)
 
+#Creating a sparse matrix bag of word
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(X_train)
+#print(X_train_counts)
 
+#Create a tf-idf matrix from X_train_counts
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+#print(X_train_tfidf)
 
+
+#Applying naive bay's model to the dataset
+#fitting input and targe into the model
+from sklearn.naive_bayes import MultinomialNB 
 clf = MultinomialNB().fit(X_train_tfidf, y_train)
-
 
 #Calculate the accuracy of the model along with the classification report
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.metrics import confusion_matrix
- 
+
 y_pred = clf.predict(count_vect.transform(X_test))
 conf_mat = confusion_matrix(y_test, y_pred)
 print(accuracy_score(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 
-
+#Plotting the confusion matrix 
 import seaborn as sns
 fig, ax = plt.subplots(figsize=(10,10))
 sns.heatmap(conf_mat, annot=True, fmt='d',
@@ -86,7 +92,7 @@ plt.ylabel('Actual')
 plt.xlabel('Predicted')
 plt.show()
 
-
+#check the model 
 print(clf.predict(count_vect.transform(["Harveys Crunchy  Creame Gourmet Delicacies Cream Wafer Biscuit 110 g Pouch Pack  Chocolate Flavoured "])))
 
 
@@ -96,6 +102,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import cross_val_score
+#Create a list with required model name 
 models = [
     RandomForestClassifier(n_estimators=200, max_depth=3, random_state=0),
     LinearSVC(),
@@ -103,7 +110,7 @@ models = [
     LogisticRegression(random_state=0),
 ]
 #Assing the level of cross validation to find the best accuracy
-CV = 5
+CV = 5  #cross validation number here it os 5 
 cv_df = pd.DataFrame(index=range(CV * len(models)))
 entries = []
 for model in models:
@@ -120,13 +127,13 @@ sns.stripplot(x='model_name', y='accuracy', data=cv_df,
               size=8, jitter=True, edgecolor="gray", linewidth=2)
 plt.show()
 
-
+#finding out the accuracy mean for all the classification model 
 cv_df.groupby('model_name').accuracy.mean()
 
 svc = LinearSVC().fit(X_train_tfidf, y_train)
 print(svc.predict(count_vect.transform(["Harveys Crunchy  Creame Gourmet Delicacies Cream Wafer Biscuit 110 g Pouch Pack  Chocolate Flavoured "])))
 
-#Applying SVC to the dataset 
+#Applying SVC to the dataset as it has the highest accuracy
 model_svc = LinearSVC()
 X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.33, random_state=0)
 model_svc.fit(X_train, y_train)
@@ -144,11 +151,11 @@ plt.show()
 print(accuracy_score(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 
+
+
 #Applying Naive bays 
-
-
 clf = MultinomialNB()
-X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.33, random_state=0)
+#X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.33, random_state=0)
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 
